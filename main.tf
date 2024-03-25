5a64b353f92cafc291ac455027beca325290454e
Andreas Kowasch <andreas.kowasch@qaware.de>
2024-03-25 13:36:50 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      # https://github.com/hashicorp/terraform-provider-azurerm/releases
+      source  = "hashicorp/azurerm"
+      version = "3.97.1"
+    }
+  }
+}
+
+################################################################################
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  skip_provider_registration = true
+}
+
+data "azurerm_client_config" "current" {}
